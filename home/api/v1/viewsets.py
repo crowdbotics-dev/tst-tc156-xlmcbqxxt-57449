25b500f4eb7e6af4b3b791e192527ed2d47f15ed
crowdbotics-dev <engineering@crowdbotics.com>
2023-01-17 16:45:54 +0000

    Update models

--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Addressarprhudcbn
+from .serializers import AddressarprhudcbnSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,8 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+class AddressarprhudcbnViewSet(viewsets.ModelViewSet):
+    serializer_class = AddressarprhudcbnSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    queryset = Addressarprhudcbn.objects.all()
